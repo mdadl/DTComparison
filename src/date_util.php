d1ffa3ee86aaea8fc41a86884dcb9b0abe2da037
mdadl <md8008@rocketmail.com>
2018-07-17 11:01:26 +0930

    Magic number replacement

--- src/date_util.php
+++ src/date_util.php
@@ -3,6 +3,18 @@
  * DateUtil class provides calculation on two different datetime values.
  */
 class DateUtil {
+    
+    const DAYS_PER_WEEK = 7;
+    const HOURS_PER_DAY = 24;
+    const MINS_PER_HOUR = 60;
+    const SECS_PER_MIN = 60;
+    const WORKING_DAYS_AS_A_YEAR = 365; // 365 working days (not calendar days) equals to 1 working year
+    const Y = 'y';
+    const DAYS = 'days';
+    const H = 'h';
+    const I = 'i';
+    const S = 's';
+    
     /**
      * This function calculates the number of days or years or hours or minutes or seconds between two given datetime.
      * 
@@ -14,20 +26,20 @@ class DateUtil {
     function number_of_days($datetime1, $datetime2, $return_type) {
         $difference = $datetime1->diff($datetime2);
         
-        if ($return_type == 'days') {
+        if ($return_type == self::DAYS) {
             return $difference->days;
         }
-        else if ($return_type == 'y') {
+        else if ($return_type == self::Y) {
             return $difference->y;
         }
-        else if ($return_type == 'h') {
-            return $difference->h + (24 * $difference->days);
+        else if ($return_type == self::H) {
+            return $difference->h + (self::HOURS_PER_DAY * $difference->days);
         }
-        else if ($return_type == 'i') {
-            return $difference->i + (60 * ($difference->h + (24 * $difference->days)));
+        else if ($return_type == self::I) {
+            return $difference->i + (self::MINS_PER_HOUR * ($difference->h + (self::HOURS_PER_DAY * $difference->days)));
         }
-        else if ($return_type == 's') {
-            return $difference->s + (60 * ($difference->i + (60 * ($difference->h + (24 * $difference->days)))));
+        else if ($return_type == self::S) {
+            return $difference->s + (self::SECS_PER_MIN * ($difference->i + (self::MINS_PER_HOUR * ($difference->h + (self::HOURS_PER_DAY * $difference->days)))));
         }
     }
     
@@ -42,7 +54,7 @@ class DateUtil {
     function number_of_weekdays($datetime1, $datetime2, $return_type) {
         $difference = $datetime1->diff($datetime2);
         
-        if ($difference->days == 0 && $return_type == 'days') return 0;
+        if ($difference->days == 0 && $return_type == self::DAYS) return 0;
         
         $begin = $datetime1->getTimestamp();
         $end = $datetime2->getTimestamp();
@@ -58,24 +70,24 @@ class DateUtil {
             if ($what_day < 6) { // 6 and 7 are weekend
                 $weekdays++;
             }
-            $begin += 86400; // +1 day
+            $begin += self::HOURS_PER_DAY * self::MINS_PER_HOUR * self::SECS_PER_MIN; // +1 day
             $i++;
         }
         
-        if ($return_type == 'days') {
+        if ($return_type == self::DAYS) {
             return $weekdays;
         }
-        else if ($return_type == 'y') {
-            return sprintf("%d", $weekdays/365);
+        else if ($return_type == self::Y) {
+            return sprintf("%d", $weekdays/self::WORKING_DAYS_AS_A_YEAR);
         }
-        else if ($return_type == 'h') {
-            return $difference->h + (24 * $weekdays);
+        else if ($return_type == self::H) {
+            return $difference->h + (self::HOURS_PER_DAY * $weekdays);
         }
-        else if ($return_type == 'i') {
-            return $difference->i + (60 * ($difference->h + (24 * $weekdays)));
+        else if ($return_type == self::I) {
+            return $difference->i + (self::MINS_PER_HOUR * ($difference->h + (self::HOURS_PER_DAY * $weekdays)));
         }
-        else if ($return_type == 's') {
-            return $difference->s + (60 * ($difference->i + (60 * ($difference->h + (24 * $weekdays)))));
+        else if ($return_type == self::S) {
+            return $difference->s + (self::SECS_PER_MIN * ($difference->i + (self::MINS_PER_HOUR * ($difference->h + (self::HOURS_PER_DAY * $weekdays)))));
         }
     }
     
@@ -90,7 +102,7 @@ class DateUtil {
     function number_of_complete_weeks($datetime1, $datetime2, $return_type) {
         $number_of_days_in_seconds = $this->number_of_days($datetime1, $datetime2, 's');
         
-        $weeks = $number_of_days_in_seconds / (7 * 24 * 60 * 60);
+        $weeks = $number_of_days_in_seconds / (self::DAYS_PER_WEEK * self::HOURS_PER_DAY * self::MINS_PER_HOUR * self::SECS_PER_MIN);
         
         return sprintf("%d", $weeks);
     }
